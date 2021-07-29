53575319b9786d654538e80a1e03cc00f327cdd1
oliverJurgen <oliver.jurgen90@gmail.com>
2021-07-29 13:34:02 +0800

    fix type errors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-// import '../styles/globals.css'
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import ThemeProvider from "../contexts/ThemeContext";
@@ -6,7 +5,7 @@ import ThemeProvider from "../contexts/ThemeContext";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <ThemeProvider theme={{}}>
+      <ThemeProvider>
         <Component {...pageProps} />
       </ThemeProvider>
     </ChakraProvider>
